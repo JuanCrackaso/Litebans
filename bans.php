45acc06fd8f9d01eca86d59fbdaa3eb7c019004b
BuildTools <unconfigured@null.spigotmc.org>
2016-02-12 10:32:14 +0100

    Fix for typo in bans.php

--- bans.php
+++ bans.php
@@ -16,11 +16,11 @@ while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
     if ($player_name === null) continue;
 
     $page->print_table_rows($row, array(
-        $page->lang->ban_player => $page->get_avatar($player_name, $row['uuid']),
-        $page->lang->ban_by     => $page->get_avatar($page->get_banner_name($row), $row['banned_by_uuid']),
-        $page->lang->ban_reason => $page->clean($row['reason']),
-        $page->lang->ban_when   => $page->millis_to_date($row['time']),
-        $page->lang->ban_expiry => $page->expiry($row),
+        $page->lang->bans_player => $page->get_avatar($player_name, $row['uuid']),
+        $page->lang->bans_by     => $page->get_avatar($page->get_banner_name($row), $row['banned_by_uuid']),
+        $page->lang->bans_reason => $page->clean($row['reason']),
+        $page->lang->bans_when   => $page->millis_to_date($row['time']),
+        $page->lang->bans_expiry => $page->expiry($row),
     ));
 }
 $page->table_end();
