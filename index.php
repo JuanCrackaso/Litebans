9afe5d37b4119006714d5167c202d30a1f7829c0
ruan <ruany@users.noreply.github.com>
2016-02-12 11:46:29 +0200

    Clean up index.php

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ $page->print_title();
 ?>
 <div class="container">
     <div class="jumbotron">
-        <div style="text-align: center;"><h2><?php echo $page->lang->index_welcome1;
-                echo $page->settings->name;
-                echo $page->lang->index_welcome2; ?></h2></div>
+        <div style="text-align: center;">
+            <h2><?php echo $page->lang->index_welcome1 . $page->settings->name . $page->lang->index_welcome2; ?></h2>
+        </div>
 
         <div style="text-align: center;"><p><?php echo $page->lang->index_allsins; ?></p></div>
     </div>
