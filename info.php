ee71bcc1b92b749435fb26f7578396b108a48568
ruan <ruany@users.noreply.github.com>
2016-02-15 19:20:40 +0200

    Fix warning info page

--- info.php
+++ info.php
@@ -78,7 +78,7 @@ class MuteInfo extends Info {
 
 class WarnInfo extends Info {
     function name() {
-        return $page->lang->info_warn_name;
+        return $this->page->lang->info_warn_name;
     }
 
     function basic_info($row, $player_name) {
