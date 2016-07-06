1eba03cc291bd0cd654f89691472c3acd0003087
ruan <ruany@users.noreply.github.com>
2016-07-06 17:35:40 +0200

    Fix column selection issue

--- inc/page.php
+++ inc/page.php
@@ -137,9 +137,12 @@ class Page {
         // Under certain versions of PHP, there is a bug with BIT columns.
         // An empty string is returned no matter what the value is.
         // Workaround: cast to unsigned.
-        $selection = "id,uuid,reason,banned_by_name,banned_by_uuid,removed_by_name,time,until,CAST(active AS UNSIGNED) AS active";
+        $selection = "id,uuid,reason,banned_by_name,banned_by_uuid,time,until,CAST(active AS UNSIGNED) AS active";
         if ($table === $this->settings->table['warnings']) {
             $selection .= ",CAST(warned AS UNSIGNED) AS warned";
+        }
+        if ($table !== $this->settings->table['kicks']) {
+            $selection .= "removed_by_name";
         }
         return $selection;
     }
